9e215e05632098517df9214238493f6fa4886c6b
Satyaki Suman <satyaki.suman@gmail.com>
2020-07-25 15:46:59 +0530

    added functionality to retrieve user's number

--- whatsapp-bot/app.py
+++ whatsapp-bot/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
+import logging
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
 #POST route to fetch messages from WhatsApp
 @app.route('/bot', methods=['POST'])
 def bot():
+    incoming_num = request.values.get('From','')
     incoming_msg = request.values.get('Body', '').lower()
+    app.logger.info(incoming_msg)
+    app.logger.info(incoming_num)
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
