e3efda473598ea6bb6579e4c7e962691f0ca0987
agatwalut79 <agarwalut79@gmail.com>
2020-07-25 19:48:50 +0530

    added url for retriving and destroying

--- AddComplaint/urls.py
+++ AddComplaint/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from AddComplaint import views
 
 urlpatterns = [
-    path('createComplaint/', views.createComplaint.as_view(), name= "complaintCreated")
+    path('createComplaint/', views.createComplaint.as_view(), name= "complaintCreated"),
+    path('retreive/', views.updateComplaint.as_view(), name='RetreiveUpdateDestroy')
 ]
