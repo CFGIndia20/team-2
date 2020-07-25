c5f602150d3dba87df877213f924f72dcfb38940
agatwalut79 <agarwalut79@gmail.com>
2020-07-26 03:55:07 +0530

    some change in forms of send_comlplaint.html

--- AddComplaint/models.py
+++ AddComplaint/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Complaint(models.Model):
     mobileNumber=models.BigIntegerField()
     description=models.TextField()
-    location=models.TextField()
+    location=models.CharField(max_length=256)
     timeCreated=models.DateTimeField(auto_now=True)
     timeUpdated=models.DateTimeField(auto_now=True ) 
     status=models.CharField(max_length=256, default='progress',null=True, blank=True)
