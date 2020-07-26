69775cfa4ed3a4af5da23cc1ec775cdc78e841de
Soumya-Joy <soumyaedappilly@gmail.com>
2020-07-26 05:39:26 +0530

    Added record button

--- janaagraha/static/janaagraha/js/complain.js
+++ janaagraha/static/janaagraha/js/complain.js
@@ -1,18 +1,23 @@
 // Send complain
 
 $(document).ready(function(){
-	$(".complain-btn").on("click", function(){
-		var mobile = $("#mobile").val();
-		if(!$('#mobile').val().match('[0-9]{10}'))  {
-            alert("Please put 10 digit mobile number");
-            return;
-        }
+	$("#id_mobileNumber").attr("placeholder", "Enter your Mobile No.");
+	$("#id_description").attr("placeholder", "Enter your Complaint");
+	$("#id_location").attr("placeholder", "Enter your Location");
 
-		var complain = $("#complain-text").val();
-		var location = $("#location").val();
+
+	// $(".complain-btn").on("click", function(){
+	// 	var mobile = $("#mobile").val();
+	// 	if(!$('#mobile').val().match('[0-9]{10}'))  {
+    //         alert("Please put 10 digit mobile number");
+    //         return;
+    //     }
+
+	// 	var complain = $("#complain-text").val();
+	// 	var location = $("#location").val();
 		
-		$.post("",{mobile: mobile, complain:complain, location:location}, function(msg){
-			console.log(msg);
-		});
-	});
+	// 	$.post("",{mobile: mobile, complain:complain, location:location}, function(msg){
+	// 		console.log(msg);
+	// 	});
+	// });
 });
